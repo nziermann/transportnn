46159944028c11cae25565aa0855d4ad53d2d575
Nils Ziermann <gitkraken@nilsziermann.de>
2020-05-23 17:26:20 +0200

    Add function to plot model

--- src/src/models/cnn.py
+++ src/src/models/cnn.py
@@ -545,10 +545,21 @@ def get_model_summaries(config, parameters):
     data = get_dummy_data()
     parameters_product = product_dict(**parameters)
 
-    for parameters_selection in parameters_product:
+    for idx, parameters_selection in enumerate(parameters_product):
         model = get_model(data, parameters_selection)
         print(parameters_selection)
+        model.build((15, 64, 128, 1))
         model.summary()
+
+        from tensorflow.python.keras.layers import wrappers
+        from tensorflow.python.keras.engine import network
+        print(f'Network: {type(model)}')
+        for layer in model._layers:
+            print(f'Layer: {type(layer)}')
+            print(f'Layer is wrapper: {isinstance(layer, wrappers.Wrapper)}')
+            print(f'Layer is network: {isinstance(layer, network.Network)}')
+
+        plot_model(model, expand_nested=True, to_file=f'{config["job_dir"]}/model_{idx}.png')
 
 
 def product_dict(**kwargs):
